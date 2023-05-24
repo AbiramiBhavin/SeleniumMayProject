0ebf836e9872928a55685b1b204576d741851f9e
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-05-24 20:34:47 +0530

    open new broser
    new broswer

--- src/day01/program1runnewbroswer.java
+++ src/day01/program1runnewbroswer.java
@@ -2,14 +2,16 @@ package day01;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class program1runnewbroswer {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		WebDriver driver;
-		driver=new ChromeDriver();
+		WebDriver driver;// double click on warning symbol in this line to import
+		driver=new EdgeDriver(); // double click on warning symbol in this line to import
 		driver.get("https://www.google.com/");
+		
 
 	}
 
