152b2bd6580db0490c2d02759d3ab9a4b03a9e91
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-05-24 20:10:52 +0530

    firstbrowseropenonselenium
    opening first web page in selenium

---
+++ src/day01/program1runnewbroswer.java
@@ -0,0 +1,16 @@
+package day01;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class program1runnewbroswer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+
+	}
+
+}
