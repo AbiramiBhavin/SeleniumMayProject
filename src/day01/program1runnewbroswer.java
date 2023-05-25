5612ef04e6185d4b97a645f4f75d84174006b34b
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-05-25 19:31:23 +0530

    program new browser
    new browser

--- src/day01/program1runnewbroswer.java
+++ src/day01/program1runnewbroswer.java
@@ -7,9 +7,10 @@ import org.openqa.selenium.edge.EdgeDriver;
 public class program1runnewbroswer {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//web driver is used to drive the application, through this we can connect to broswer
 		WebDriver driver;// double click on warning symbol in this line to import
 		driver=new EdgeDriver(); // double click on warning symbol in this line to import
+		// we can also make use of edge, firefox what we have in our system
 		driver.get("https://www.google.com/");
 		
 
