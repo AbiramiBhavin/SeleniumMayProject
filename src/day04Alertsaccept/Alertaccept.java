1c75a62601f7593f1b4a7e236fdcdbd882fb9d13
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-05-30 20:44:03 +0530

    Alertaccept
    accept in alert using xpath contains

---
+++ src/day04Alertsaccept/Alertaccept.java
@@ -0,0 +1,22 @@
+package day04Alertsaccept;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alertaccept {
+
+	public static void main(String[] args) {
+		WebDriver driver;
+		driver =new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Alerts.html");
+		driver.findElement(By.xpath("//button[contains(text(),'click the button to display an  alert box' )]")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		
+		
+
+	}
+
+}
