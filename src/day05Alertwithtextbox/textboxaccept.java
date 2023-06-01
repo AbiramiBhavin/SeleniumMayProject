f8ee3162208dc2e2101b7d6ee9c7b78ea476c137
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-06-01 20:51:03 +0530

    textbox accept
    textbox accept

---
+++ src/day05Alertwithtextbox/textboxaccept.java
@@ -0,0 +1,24 @@
+package day05Alertwithtextbox;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class textboxaccept {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver;
+		driver=new ChromeDriver();
+		driver.get("https://demo.automationtesting.in/Alerts.html");
+		driver.findElement(By.xpath("//a[contains(text(),'Alert with Textbox ')]")).click();
+		driver.findElement(By.xpath("//button[contains(text(),'click the button to demonstrate the prompt box ')]")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().sendKeys("Java");
+		driver.switchTo().alert().accept();
+		
+		
+
+	}
+
+}
