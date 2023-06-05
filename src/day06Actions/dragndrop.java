4afa437cf1fb7d58ec39f17b2b75eec4983497fd
ABIRAMIRAVICHANDARANravi <abiramiravichandaran@gmail.com>
2023-06-05 21:58:54 +0530

    Drag and drop
    note:
    Only certain blocks are permitted to be dropped in specific cells in the table

--- src/day06Actions/dragndrop.java
+++ src/day06Actions/dragndrop.java
@@ -1,4 +1,5 @@
 package day06Actions;
+import java.io.*;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -8,12 +9,12 @@ import org.openqa.selenium.interactions.Actions;
 
 public class dragndrop {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
 		WebDriver driver;
 		driver=new ChromeDriver();
 		driver.get("https://demo.guru99.com/test/drag_drop.html");
-		WebElement src=driver.findElement(By.xpath("//li[@id='credit2']"));
+		WebElement src=driver.findElement(By.xpath("//li[@id='credit2']"));//webelement is used for storing xpath
 		WebElement target=driver.findElement(By.xpath("//ol[@id='bank']"));
 		Actions action=new Actions(driver);
 		action.dragAndDrop(src, target).build().perform();
@@ -22,14 +23,18 @@ public class dragndrop {
 		target=driver.findElement(By.xpath("//ol[@id='amt7']"));
 		action.dragAndDrop(src, target).build().perform();
 		//My tries are below for the rest tabs
-		WebElement src1=driver.findElement(By.xpath("//li[@id='credit4']"));
-		WebElement target1=driver.findElement(By.xpath("//ol[@id='loan']"));
-		//WebElement target1=driver.findElement(By.xpath("//ol[@id='loan']"));
-		action.dragAndDrop(src1, target1).build().perform();
+		//Only certain blocks are permitted to be dropped in specific cells in the table
+		//I used Sales here
 		
-		WebElement src2=driver.findElement(By.xpath("//li[@id='fourth']"));
-		WebElement target2=driver.findElement(By.xpath("//ol[@id='amt8']"));
-		action.dragAndDrop(src2, target2).build().perform();
+		WebElement srcc=driver.findElement(By.xpath("//li[@id='credit1']"));//webelement is used for storing xpath
+		WebElement targett=driver.findElement(By.xpath("//ol[@id='loan']"));
+		Actions action1=new Actions(driver);
+		action1.dragAndDrop(srcc, targett).build().perform();
+		
+		
+		srcc=driver.findElement(By.xpath("//li[@id='fourth']"));
+		targett=driver.findElement(By.xpath("//ol[@id='amt8']"));
+		action1.dragAndDrop(srcc, targett).build().perform();
 		
 		
 		
